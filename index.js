37a8c11f81cf2a12fec77e94cac4f572d2f0ba50
Nyssa Keller <nyssa1@gmail.com>
2017-10-26 11:19:51 -0600

    cleaned up css and js

--- index.js
+++ index.js
@@ -24,19 +24,6 @@ function cardObject() {
     };
 }
 
-$('.save-btn').on('click', function(event) {
-    event.preventDefault();
-    console.log('#body-input');
-    console.log('#title-input');
-    if ($('#title-input').val() === "" || $('#body-input').val() === "") {
-       return false;
-    };  
-    numCards++;
-    $( ".bottom-box" ).prepend(newCard('card' + numCards, $('#title-input').val(), $('#body-input').val(), qualityVariable)); 
-    localStoreCard();
-    $('form')[0].reset();
-});
-
 $.each(localStorage, function(key) {
     var cardData = JSON.parse(this);
     numCards++;
@@ -48,12 +35,22 @@ var localStoreCard = function() {
     localStorage.setItem('card' + numCards  , cardString);
 }
 
+$('.save-btn').on('click', function(event) {
+    event.preventDefault();
+    if ($('#title-input').val() === "" || $('#body-input').val() === "") {
+       return false;
+    };  
+
+    numCards++;
+    $( ".bottom-box" ).prepend(newCard('card' + numCards, $('#title-input').val(), $('#body-input').val(), qualityVariable)); 
+    localStoreCard();
+    $('form')[0].reset();
+});
 
 $(".bottom-box").on('click', function(event){
     var currentQuality = $($(event.target).siblings('p.quality').children()[0]).text().trim();
     var qualityVariable;
 
-    // Case 1: user clicks up or down
     if (event.target.className === "upvote" || event.target.className === "downvote"){
 
         if (event.target.className === "upvote" && currentQuality === "plausible"){
@@ -79,7 +76,6 @@ $(".bottom-box").on('click', function(event){
             qualityVariable = "genius";
         }
 
-
     var cardHTML = $(event.target).closest('.card-container');
     var cardHTMLId = cardHTML[0].id;
     var cardObjectInJSON = localStorage.getItem(cardHTMLId);
@@ -89,44 +85,18 @@ $(".bottom-box").on('click', function(event){
 
     var newCardJSON = JSON.stringify(cardObjectInJS);
     localStorage.setItem(cardHTMLId, newCardJSON);
-
     }
-
-    // Case 2: user click on delete    
-     else if (event.target.className === "delete-button") {
+   
+    else if (event.target.className === "delete-button") {
         var cardHTML = $(event.target).closest('.card-container').remove();
         var cardHTMLId = cardHTML[0].id;
         localStorage.removeItem(cardHTMLId);
     }
 });
+      
+}
 
 
-// An event listener on the bottom half. If the target of
-// the click is upvote or downvote, change the value of 
-// the qualityVariable variable accordingly.
-
-
-   
-
-
-
-// $('delete-button')on.('click',function(){
-//delete function goes here //
-// });
-
-// 
-// User Flows
-// 1. Users see a list of all existing ideas.
-//     - All ideas stay on the page at refresh, including title, body, and quality.
-
-// Adding a New Idea
-
-// 2. The idea should be added localStorage.
-//     - JSON?? Look it up!
-
-// Deleting an Existing Idea.
-
-// 2. The idea should be removed from localStorage.
 
 
 
